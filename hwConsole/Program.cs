e9d35e8b9323d69201cffcf02708481b12ce063d
Ксения Тапок <soulssfg@gmail.com>
2021-11-04 11:36:50 +0300

    Add project files.

---
+++ hwConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hwConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
